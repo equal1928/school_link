a095b79cf2213dc085c230f10fae159f03a20b40
m.neustroev <m.neustroev@redkit-lab.ru>
2023-11-23 20:45:47 +0500

    Реализация фильтра по названию школ

--- src/models/SchoolModel.tsx
+++ src/models/SchoolModel.tsx
@@ -1,8 +1,4 @@
 export interface SchoolModel {
     id: number;
-    photo: string;
-    title: string;
-    features: string[];
-    address: string;
-    link: string;
+    name: string;
 }
---
+++ src/models/SchoolModelCard.tsx
@@ -0,0 +1,8 @@
+export interface SchoolModelCard {
+    id: number;
+    photo: string;
+    name: string;
+    features: string[];
+    address: string;
+    link: string;
+}
